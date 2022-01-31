aba1ed9e8f387a88c1fd6813d145611f951204e7
slackers <severin.gebesmair@gmx.at>
2022-01-31 22:05:22 +0100

    nightly: reworked testing walls; setup and plan for the wall management and creation

---
+++ Source/KaijuGJ2022/WallCreator.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WallCreator.h"
+
+// Sets default values
+AWallCreator::AWallCreator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWallCreator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Instantiate all managers at positions given in some array
+	//have new array with references to all the managers
+	
+	//Get Reference to the Camera and to the Player
+}
+
+// Called every frame
+void AWallCreator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Get current player and camera position
+
+	//calculate ray and then "box/sphere cast?"
+
+	//Send hit message to all managers (WasHit(FHitResult))
+}
+
